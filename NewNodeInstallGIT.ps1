c946aeecec8a5480610e3d91a191c5b90775cc18
Ben Gelens <ben.gelens@inovativ.nl>
2015-04-13 22:01:00 +0200

    fixed some issues

--- NewNodeInstallGIT.ps1
+++ NewNodeInstallGIT.ps1
@@ -1,9 +1,10 @@
 ﻿Find-Package git.install | Install-Package -Force
 Find-Package poshgit | Install-Package -Force
 [System.Environment]::SetEnvironmentVariable('Path', $env:path + ';C:\Program Files (x86)\Git\bin', 'Machine')
-$env:path = [System.Environment]::GetEnvironmentVariable('Path')
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 $POSHGITProfile = (Get-ChildItem -Filter profile.example.ps1 -Path c:\tools -Recurse).FullName
 if (!(Test-Path $profile.AllUsersAllHosts)) {
     New-Item $profile.AllUsersAllHosts | Out-Null
 }
-". $POSHGITProfile" | Out-File -FilePath $profile.AllUsersAllHosts -Append -NoClobber
+"`r`n. $POSHGITProfile" | Out-File -FilePath $profile.AllUsersAllHosts -Append -NoClobber -Encoding ascii
+. $POSHGITProfile
