6320bff0b46645b83a026ff87425d159231a4a47
Ben Gelens <ben.gelens@inovativ.nl>
2015-04-14 21:49:02 +0200

    added DSC Pull Module Populate script

---
+++ PopulateDSCPullModules.ps1
@@ -0,0 +1,16 @@
+Find-DscResource | Select-Object -Property ModuleName -Unique | %{Install-Module $_.ModuleName -Force}
+$PullServerModulesPath = 'C:\Program Files\WindowsPowerShell\DscService\Modules'
+Get-DscResource | Select-Object -Property Module -Unique | %{
+    if ($_.Module -match '^[C,X]') {
+        $Module = Get-Module $_.Module -ListAvailable
+        $Manifest = Test-ModuleManifest $Module.Path
+        $ArchiveName = $Manifest.Name + '_' + $Manifest.Version.ToString() + '.zip'
+        if (!(Test-Path "$PullServerModulesPath\$ArchiveName")) {
+            Write-Verbose -Message "Adding $($Manifest.name) with version $($Manifest.Version.ToString()) to DSC Pull Server Modules repository" -Verbose
+            #clear readonly (compress archive seems to break with access denied if this is set)
+            Get-ChildItem $Module.ModuleBase -Recurse | %{attrib.exe -R $_.FullName}
+            Compress-Archive -Path $module.ModuleBase -DestinationPath "$PullServerModulesPath\$ArchiveName" -Force
+            New-DSCCheckSum -Path $PullServerModulesPath\$ArchiveName -Force
+        }
+    }
+}
