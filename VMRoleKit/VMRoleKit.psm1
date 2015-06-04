b12fe6772bc2b86421cf5546ef086e3a8cdc88ae
Ben Gelens <ben.gelens@inovativ.nl>
2015-06-04 09:10:55 +0200

    Split functions

--- VMRoleKit/VMRoleKit.psm1
+++ VMRoleKit/VMRoleKit.psm1
@@ -17,9 +17,7 @@ function Get-VMRoleResourceDefinition {
             }
             
         })]
-        [System.io.FileInfo] $ResourceDefinitionPackagePath,
-
-        [Switch] $IncludeViewDefinition
+        [System.io.FileInfo] $ResourceDefinitionPackagePath
     )
     Begin {
         
@@ -46,17 +44,10 @@ function Get-VMRoleResourceDefinition {
             Write-Verbose -Message "Creating temp directory to expand ResDefPKG in: $ExpandDir"
             New-Item -Path $TempDir -Name $ResourceDefinitionPackagePath.BaseName -ItemType Directory | Out-Null
 
-            Write-Verbose -Message "Expanding $ResourceDefPKG into $ExpandDir"
-            [io.compression.zipfile]::ExtractToDirectory($Path, $ExpandDir)
+            Expand-Archive -Path $Path -ExpandDir $ExpandDir
 
             Write-Verbose -Message 'Finding ResDef file and converting JSON to PSObject'
             $ResDef = Get-ChildItem *.resdef -Path $ExpandDir | Get-Content | Out-String | ConvertFrom-Json
-
-            if ($IncludeViewDefinition) {
-                Write-Verbose -Message 'IncludeViewDefinition Switch enabled, adding ViewDef property'
-                $ViewDef = Get-ChildItem *.viewdef -Path $ExpandDir | Get-Content | Out-String | ConvertFrom-Json
-                Add-Member -InputObject $ResDef -Name ViewDef -Value $ViewDef -MemberType NoteProperty -Force
-            }
 
             Write-Verbose -Message "Removing expand directory: $ExpandDir"
             Remove-Item $ExpandDir -Force -Recurse
@@ -66,10 +57,99 @@ function Get-VMRoleResourceDefinition {
         catch {
             Write-Error -Message $_.exception.message -ErrorAction Continue
         }
+        finally {
+            if (Test-Path -Path $ExpandDir) {
+                Write-Verbose -Message "Removing expand directory: $ExpandDir"
+                Remove-Item $ExpandDir -Force -Recurse
+            }
+        }
     }
     End {
 
     }
 }
-#Get-VMRoleResourceDefinition -Verbose -ResourceDefinitionPackagePath .\import\RABO_PROD_W2012R2_DEV_1001.resdefpkg -IncludeViewDefinition
-#dir .\import -File | Get-VMRoleResourceDefinition -IncludeViewDefinition -Verbose
+#Get-VMRoleResourceDefinition -Verbose -ResourceDefinitionPackagePath .\import\RABO_PROD_W2012R2_DEV_1001.resdefpkg
+#dir .\import -File | Get-VMRoleResourceDefinition -Verbose
+
+function Get-VMRoleViewDefinition {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({
+            if (Test-Path -Path $_.fullname){
+                $true
+            }
+            elseif ($_ -like '.\*') {
+                Test-Path (Resolve-Path $_)
+            }
+            
+        })]
+        [System.io.FileInfo] $ResourceDefinitionPackagePath
+    )
+    Begin {
+
+    }
+    Process {
+        if ($ResourceDefinitionPackagePath -like '.\*') {
+            $ResourceDefinitionPackagePath = Get-Item (Resolve-Path $ResourceDefinitionPackagePath)
+        }
+        $Path = $ResourceDefinitionPackagePath.FullName
+        $ResourceDefPKG = $ResourceDefinitionPackagePath.Name
+        $TempDir = [System.IO.Path]::GetTempPath()
+        $ExpandDir = Join-Path -Path $TempDir -ChildPath $ResourceDefinitionPackagePath.BaseName
+
+        Write-Verbose -Message "Processing $ResourceDefinitionPackagePath"
+
+        $ErrorActionPreference = 'Stop'
+
+        try {
+            Write-Verbose -Message "Checking if expand directory $ExpandDir already exits"
+            if (Test-Path $ExpandDir) {
+                Write-Verbose -Message 'Removing expand directory as it already exists'
+                Remove-Item $ExpandDir -Recurse -Force
+            }
+            Write-Verbose -Message "Creating temp directory to expand ResDefPKG in: $ExpandDir"
+            New-Item -Path $TempDir -Name $ResourceDefinitionPackagePath.BaseName -ItemType Directory | Out-Null
+
+            Expand-Archive -Path $Path -ExpandDir $ExpandDir
+
+            Write-Verbose -Message 'Finding ViewDef file and converting JSON to PSObject'
+            $ViewDef = Get-ChildItem *.viewdef -Path $ExpandDir | Get-Content | Out-String | ConvertFrom-Json
+
+            Write-Output -InputObject $ViewDef
+        }
+        catch {
+            Write-Error -Message $_.exception.message -ErrorAction Continue
+        }
+        finally {
+            if (Test-Path -Path $ExpandDir) {
+                Write-Verbose -Message "Removing expand directory: $ExpandDir"
+                Remove-Item $ExpandDir -Force -Recurse
+            }
+        }
+    }
+    End {
+
+    }
+
+}
+#Get-VMRoleViewDefinition -Verbose -ResourceDefinitionPackagePath .\import\RABO_PROD_W2012R2_DEV_1001.resdefpkg
+#dir .\import -File | Get-VMRoleViewDefinition -Verbose
+
+#Helper function for pre WMF5
+function Expand-Archive {
+    param (
+        [String] $Path,
+
+        [String] $ExpandDir
+    )
+
+    Write-Verbose -Message "Expanding $Path into $ExpandDir"
+    [io.compression.zipfile]::ExtractToDirectory($Path, $ExpandDir)
+}
+
+Export-ModuleMember -Function *-VMRole*
